78d31340c34e4ed7368fbf8162c807cd35aa1dd7
Arianajaramillo17 <arianajaramillo17@gmail.com>
2024-03-06 23:11:12 -0500

    j

---
+++ Service/Cotizacion/Service.Cotizacion.Application/Repositories/ICotizacionRepository.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Application;
+using Entity = Service.Cotizacion.
+
+
+namespace Service.Cotizacion.Application.Repositories
+{
+    public interface ICotizacionRepository:IRepositoryBase<Enty>
+    {
+        
+    }
+}
